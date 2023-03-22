fc93dd2204195930ce97b00df30de8088a34c965
DELL <DELL@DESKTOP-KLJI07S>
2023-03-23 01:40:24 +0700

    mengelola Array

---
+++ latihan5/latihan5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[5];
+	cout << "mengisi array" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "Data ke- " << (i + 1) << ": ";
+		cin >> a[i];
+	}
+	cout << "Membaca Array" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "Data ke- " << (i + 1) << ": " << a[i] << endl;
+	}
+}
