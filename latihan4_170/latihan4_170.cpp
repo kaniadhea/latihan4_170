a4109f0344f16368d87159a643e3674dad2c7cd3
DELL <DELL@DESKTOP-KLJI07S>
2023-03-23 01:06:33 +0700

    masukan luas persegi, lingkaran,segitiga

--- latihan4_170/latihan4_170.cpp
+++ latihan4_170/latihan4_170.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
-using namespace s
+using namespace std;
+
+float LuasPersegi(float p, float 1) {
+	return p * 1;
+}
+float LuasLingkaran(float r) {
+	return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+	return 0.5 * a + t;
+}
+
+int main() {
+	float panjang, lebar, jejari, alas, tinggi;
+	int pilihan;
